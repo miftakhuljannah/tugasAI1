57df2b416e34882f28f4404d02fa00898c4a4573
Miftah <miftakhuljannah3010@gmail.com>
2018-09-23 12:37:08 +0700

    v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,25 @@ import java.util.Random;
 import static java.lang.Math.*;
 
 public class Main {
-
     private static double x, y;
-
     private static double hitung(double x, double y) {
         return -abs(sin(x)*cos(y)*exp(abs(1 - sqrt(pow(x,2)+pow(y,2))/PI)));
     }
-
     public static void main(String[] args) {
-        x = 1;
-        y = 2;
+        // menetapkan nilai awal dan suhu awal
+        x = 4;
+        y = 4;
+        System.out.println("Parameter awal (x,y):(" + x + "," + y + "). ");
         double suhu = 1000;
-
-        double solusi = 0;
+        double solusi = 100;
         double delta = 100;
         double iterasi = 0;
+        double xakhir = 0, yakhir = 0;
+
         int kali = 0;
+        //menghitung solusi sementara dan menjadikannya best so far.
         double solusiSementara = hitung(x,y);
-
-
+        //lakukan perulangan
 	    while (kali <= 4 && suhu > 0 ){
 	        iterasi++;
 	        if (delta<=pow(10,-11)){
@@ -31,45 +31,51 @@ public class Main {
             }else{
 	            kali=0;
             }
+            // Ubah XY
             ubahXY();
+	        // Hitung solusi sementara
 	        solusi = hitung(x,y);
+	        // Jika solusi baru lebih baik dari solusi sementara
 	        if (solusi < solusiSementara){
+	            // Hitung delta, solusi sesementara diganti deengan yang baru.
 	            delta = abs(solusi-solusiSementara);
 	            solusiSementara = solusi;
-            }
+	            xakhir = x;
+	            yakhir = y;
+            } // Jika solusi baru tidak lebih baik dari solusi sementara
             else{
+                // Hitung delta
                 double deltaSementara = solusi - solusiSementara;
                 if (exp(-deltaSementara/suhu) > random(0,1)) {
+                    // Jika rumus melebihi nilai random dari 0...1
+                    // update nilai delta dan solusi sementara
                     delta = abs(deltaSementara);
                     solusiSementara = solusi;
+                    xakhir = x;
+                    yakhir = y;
                 }
             }
+            // 4. dinginkan suhu
             suhu -= 0.0000001;
         }
-        System.out.println("(x,y): (" + x + "," + y + ")." + "[solusi]:" + solusi + ". [iterasi]:" + iterasi + ". [suhu]:" + suhu);
+        // print solusi
+        System.out.println("(x,y): (" + xakhir + "," + yakhir + ")." + "[solusi]:" + solusi + ". [iterasi]:" + iterasi + ". [suhu]:" + suhu);
     }
 
     private static void ubahXY (){
         double alpha = random(-0.00001, 0.00001);
-        double hasil[] = {
-                hitung(x+alpha, y),
-                hitung(x, y+alpha),
-                hitung(x-alpha, y),
-                hitung(x,y-alpha),
-                hitung(x+alpha, y+alpha),
-                hitung(x-alpha, y-alpha),
-                hitung(x+alpha, y-alpha),
-                hitung(x-alpha, y+alpha)
+        double hasil[] = {hitung(x+alpha, y), hitung(x, y+alpha), hitung(x-alpha, y), hitung(x,y-alpha), hitung(x+alpha, y+alpha), hitung(x-alpha, y-alpha), hitung(x+alpha, y-alpha), hitung(x-alpha, y+alpha)
         };
         double min = hasil[0];
-        int x = 0;
+        int j = 0;
         for (int i = 0; i<8; i++){
             if (hasil[i] < min){
                 min = hasil[i];
-                x = i;
+                j = i;
             }
         }
-        switch (x){
+
+        switch (j){
             case 0:
                 x+=alpha;
                 break;
@@ -98,7 +104,6 @@ public class Main {
                 x-=alpha;
                 y+=alpha;
                 break;
-
         }
     }
 
